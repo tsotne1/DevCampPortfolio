4fcaa4fb23ada0ee149bae112155f1dbf2327173
tsotne1 <tsotne.mazmishvili.1@btu.edu.ge>
2020-02-19 15:15:35 +0400

    created seeds file to sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+  Blog.create!(
+    title: "My blog post #{blog}",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+       nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+       reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+       pariatur. Excepteur sint occaecat cupidatat non proident,
+    sunt in culpa qui officia deserunt mollit anim id est laborum."
+  )
+end
+
+puts "10 blog posts created"
+
+5.times do |skill|
+  Skill.create!(
+    title: "Rails #{skill}",
+    percent_utilized: 15
+  )
+end
+
+puts "5 skills created"
+
+9.times do |portffolio_item|
+  Portfolio.create!(
+    title: "Portfolio ttitle #{portffolio_item}",
+    subtitle: "My great service",
+    body: "Lorem Ipsum is simply dummy text of the printing
+          and typesetting industry. Lorem Ipsum has been the industry's standard
+          dummy text ever since the 1500s, when an unknown printer took a
+           galley of type and scrambled it to make a type specimen book" ,
+    main_image: "http://via.placeholder.com/5%10C/O%20https://placeholder.com/",
+    thumb_image: "http://via.placeholder.com/5%5C/O%20https://placeholder.com/" ,
+  )
+end
+
+puts "9 portfolio items created"
